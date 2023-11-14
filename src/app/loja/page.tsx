06a63a1fb8ee25e5ea197193d34f250ff6248f5c
wesley <wesleyaleixo0950@gmail.com>
2023-11-14 16:11:16 -0300

    Clerk

--- src/app/loja/page.tsx
+++ src/app/loja/page.tsx
@@ -1,10 +1,37 @@
-import Header from "@/components/header";
-import "./lojastyle.css"
+import { currentUser } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
 
-export default function Public() {
-  return (
-    <div>
-      <Header />  
-    </div>
+const prisma = new PrismaClient();
+
+async function findUserByEmail(email) {
+  return prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+}
+
+export default async function Page() {
+  const user = await currentUser();
+
+  if (!user) return <div>Not logged in</div>;
+
+  const existingUser = await findUserByEmail(
+    user.emailAddresses[0].emailAddress
   );
+
+  if (existingUser) {
+    // O usuário já existe no banco de dados
+    return <div>Hello {user.emailAddresses[0].emailAddress}</div>;
+  } else {
+    // O usuário não existe, então criamos um novo registro
+    const newUser = await prisma.user.create({
+      data: {
+        name: user.firstName,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+
+    return <div>Hello {newUser.email}</div>;
+  }
 }
