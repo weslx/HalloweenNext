8a4c683c90c7699d3538fdb2d82669cb83babc79
wesley <wesleyaleixo0950@gmail.com>
2023-11-20 16:34:29 -0300

    Removed tailwind + css colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
+import style from "./style.module.css";
 
 export default function Public() {
   return (
-    <div className="h-screen font-[3vh] font-[Sweet Scream] bg-[rgb(40,40,40)] flex items-center justify-center">
-      <div className="w-[50vw] h-[50vh] bg-[rgb(105,0,0)] rounded-[10%] border-[1vh] border-[rgb(255,140,0)] animate-colorchange">
-        <div className="grid items-center justify-center text-[rgb(255,140,0)]">
-          <h1>Teste login</h1>
-          <p>faça login para continuar</p>
+    <div className={style.fundo}>
+      <div className={style.borda}>
+        <div>
+          <h1>Faça login para continuar</h1>
+          <p>Voce autoriza a coleta de dados</p>
           <Link href={"/loja"}>
-            <button className="w-[20vh] h-[10vh] bg-[rgb(255,140,0)] text-white rounded-[10vh]">Entrar</button>
+            <button className={style.button}>Entrar</button>
           </Link>
         </div>
       </div>
